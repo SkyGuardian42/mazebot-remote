91376be39f898850e4a419fa2d28777991b16de3
malte <skyguardian42@gmail.com>
2018-12-15 00:01:50 +0100

    gpio pins assigned

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Mazebot Webinterface
 > Fernsteuerung des Mazebot durch ein Webinterface
+l-fwd 29
+r-fwd 32
+l-bwd 31
+r-bwd 33
--- routes/motors.js
+++ routes/motors.js
@@ -2,18 +2,18 @@ const express = require('express'),
       router  = express.Router();
 
 let gpio;
-const pins = [5, 6, 7, 8]; // l-fwd, r-fwd, l-bwd, r-bwd
+const pins = [29, 32, 31, 33]; // l-fwd, r-fwd, l-bwd, r-bwd
 
-//if(process.env.PI) {
+if(process.env.PI) {
   gpio = require('rpi-gpio');
   console.log('gpio active');
   pins.forEach(pin => gpio.setup(pin, gpio.DIR_OUT));
-//}
+}
 
 
 router.post('/', function(req, res, next) {
   
-  //gpio.write(pins[req.body.target], req.body.status)
+  gpio.write(pins[req.body.target], req.body.status)
   console.log(req.body);
   res.send('ok');
 });
